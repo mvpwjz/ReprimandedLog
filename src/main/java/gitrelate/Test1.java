f9769924ce2d1652601188e8bbfd4520fe776b69
WenJunZhemvp <wjzmvp@yeah.net>
2022-06-03 21:26:30 +0800

    mvpwjz 第3次提交

--- src/main/java/gitrelate/Test1.java
+++ src/main/java/gitrelate/Test1.java
@@ -2,7 +2,7 @@ package gitrelate;
 
 public class Test1 {
     public static void main(String[] args) {
-        System.out.println("WenJunZhe 第一次提交");
+        System.out.println("mvpwjz 第4次提交");
         System.out.println("mvpwjz 第一次提交");
     }
 }
