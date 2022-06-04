3302e1345ba3cdc4b221d480ec544aff52151ba1
WenJunZhemvp <wjzmvp@yeah.net>
2022-06-04 09:06:44 +0800

    mvpwjz 第6次提交

--- src/main/java/gitrelate/Test1.java
+++ src/main/java/gitrelate/Test1.java
@@ -2,6 +2,7 @@ package gitrelate;
 
 public class Test1 {
     public static void main(String[] args) {
+        System.out.println("分支修改");
         System.out.println("wjzmvp");
         System.out.println("wjz,nb");
         System.out.println("wenjunzhe 第1次提交");
