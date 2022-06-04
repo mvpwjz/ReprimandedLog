b1846527a9ae155b5f469864e0879dd3e68e2de9
WenJunZhemvp <wjzmvp@yeah.net>
2022-06-04 20:24:51 +0800

    wjz nb plus

--- src/main/java/gitrelate/Test1.java
+++ src/main/java/gitrelate/Test1.java
@@ -2,7 +2,7 @@ package gitrelate;
 
 public class Test1 {
     public static void main(String[] args) {
-        System.out.println("wjz, nb");
+        System.out.println("wjz, nb plus");
         System.out.println("wjz, mvp");
     }
 }
