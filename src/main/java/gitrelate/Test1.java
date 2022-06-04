29c2dc3db1f738d8ae7c7fc094cf7a1e8e02aa02
WenJunZhemvp <wjzmvp@yeah.net>
2022-06-04 13:55:03 +0800

    mvpwjz1提交

--- src/main/java/gitrelate/Test1.java
+++ src/main/java/gitrelate/Test1.java
@@ -2,6 +2,7 @@ package gitrelate;
 
 public class Test1 {
     public static void main(String[] args) {
-        System.out.println("wjz,mvp");
+        System.out.println("wjz, nb");
+        System.out.println("wjz, mvp");
     }
 }
